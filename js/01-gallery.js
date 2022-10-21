88ea02e08fea0b0aaa0c808733a679177c6366bc
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-21 20:11:03 +0300

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.innerHTML = addGalleryMarkup;
+console.log(addGalleryMarkup);
 console.log(galleryItems);
+console.log(galleryRef);
