ebd43064fd323335db35dca7a97c9d3a55367aa9
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-10-24 17:10:51 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+// const basicLightbox = window.basicLightbox;
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const galleryRef = document.querySelector(".gallery");
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", addGalleryMarkup);
+
+galleryRef.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.open();
+}
 
 console.log(galleryItems);
